eca84476b7b7aea3b4a832a1ef84e54ee2707eae
fonsecaBarreto <lucasbfonte@gmail.com>
2021-10-22 00:30:05 -0300

    fix react router-dom

--- server.js
+++ server.js
@@ -10,6 +10,11 @@ const PORT = process.env.PORT || 8080
 
 app.use('/', express.static(path.join(__dirname,'build')))
 
+app.get('/*', function (req, res) {
+     res.sendFile(path.join(__dirname,'build','index.html'));
+}); 
+   
+
 app.listen(PORT, () =>
   console.log(`Listening on port ${PORT}!`)
 );
